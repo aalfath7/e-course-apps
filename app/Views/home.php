0564bf0c1268e79523fd841cae27a6f58c768334
aalfath7 <abdielhafizhy@gmail.com>
2025-03-05 10:01:18 +0700

    edit desc section

--- app/Views/home.php
+++ app/Views/home.php
@@ -4,6 +4,18 @@
 <div class="p-5 space-y-10">
     <?= $this->include('components/carousel') ?>
 
-    <h1>Home Page</h1>
+    <div class="grid grid-cols-2 gap-5 bg-blue-100">
+        <div class="flex items-center justify-center">
+            <div>
+                <h1 class="font-bold text-4xl">E Course Platform</h1>
+                <p class="text-xl">Tempat belajar berbagai ilmu pengetahuan.</p>
+            </div>
+        </div>
+        <div class="flex justify-center items-center">
+            <img class="h-44" src="<?= base_url('images/ecourse-ilust.jpg') ?>" alt="">
+        </div>
+    </div>
+
+    
 </div>
 <?= $this->endSection() ?>
